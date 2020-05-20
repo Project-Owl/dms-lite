f474fcab2f6f87dc8e5472be2f1cfe3409c1c4e1
TimoWielink <timowielink@gmail.com>
2020-05-20 18:56:01 +0200

    Add sqlwriter.py

---
+++ sqlwriter.py
@@ -0,0 +1,51 @@
+from time import gmtime, strftime
+import paho.mqtt.client as mqtt
+import sqlite3
+from sqlite3 import Error
+import json
+
+status_topic = "status"
+dbFile = "data.db"
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(status_topic)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+    result = (theTime + "\t" + str(msg.payload))
+    print(msg.topic + ":\t" + result)
+    if (msg.topic == status_topic):
+        text = json.loads(msg.payload)
+        print (json.dumps(text))
+        writeToDb(theTime, text["DeviceID"], text["MessageID"], text["Payload"], text["path"])
+    return
+
+def writeToDb(theTime, duckId, messageId, payload, path):
+    conn = sqlite3.connect(dbFile)
+    c = conn.cursor()
+    # c.execute("SELECT COUNT * FROM clusterData")
+    print ("Writing to db...")
+    try:
+        c.execute("INSERT INTO clusterData VALUES (?,?,?,?,?)", (theTime, duckId, messageId, payload, path))
+        conn.commit()
+    except Error as e:
+        print(e)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("127.0.1.1", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
