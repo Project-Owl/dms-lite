5cc680ffaffe16b7572b427ec8ba177e3f26e893
nfeuer <nickfeuer@gmail.com>
2020-05-20 20:20:29 -0400

    add db connection on node

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('data.db', (err) => {
+  if(err) {
+    console.error(err.message);
+  }
+  console.log('Connected to data.db');
+});
+
+printAllData();
 
 /* GET data route */
 router.get('/', function(req, res, next) {
   res.render('data', { title: 'Express' });
 });
+
+function printAllData() {
+  let sql = 'SELECT * FROM clusterData'
+  
+  db.all(sql, [], (err, rows) => {
+    if(err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  });
+}
 
 module.exports = router;
