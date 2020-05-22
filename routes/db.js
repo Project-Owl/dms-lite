b551e196f0cbfd32cf61685df7f8d4db9fee6dcd
nfeuer <nickfeuer@gmail.com>
2020-05-22 15:03:21 -0400

    open and close db

--- routes/db.js
+++ routes/db.js
@@ -2,21 +2,23 @@ var express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('data.db', (err) => {
-   if (err) {
-      console.error(err.message);
-   }
-   console.log('Connected to data.db');
-});
+var db;
+
+openDB();
+closeDB();
+
 
 // CREATE TABLE clusterData(time_recieved datetime, duck_id TEXT, message_id TEXT, payload TEXT, path TEXT);
 
 /* GET data route */
 router.get('/', function (req, res, next) {
-   res.render('db', {title: 'Express'});
+   res.render('db', {
+      title: 'Express'
+   });
 });
 
 router.get('/data', function (req, res, next) {
+   openDB();
    let sql = 'SELECT time_recieved, duck_id, message_id, payload FROM clusterData'
 
    db.all(sql, [], (err, rows) => {
@@ -26,13 +28,15 @@ router.get('/data', function (req, res, next) {
       rows.forEach((row) => {
          console.log(row); //For debug
       });
-      
+
       res.json(rows);
    });
-   
+
+   closeDB();
 });
 
 router.get('/data/:duckId', function (req, res, next) {
+   openDB();
    let sql = 'SELECT time_recieved, duck_id, message_id, payload, path FROM clusterData WHERE duck_Id = ?'
 
    db.all(sql, [req.params.duckId], (err, rows) => {
@@ -45,9 +49,12 @@ router.get('/data/:duckId', function (req, res, next) {
 
       res.json(rows);
    });
+
+   closeDB();
 });
 
 router.get('/data/ducks', function (req, res, next) {
+   openDB();
    let sql = 'SELECT DISTINCT duck_id FROM clusterData'
 
    db.all(sql, [], (err, rows) => {
@@ -60,9 +67,12 @@ router.get('/data/ducks', function (req, res, next) {
 
       res.json(rows);
    });
+
+   closeDB();
 });
 
 router.get('/data/latest/:count', function (req, res, next) {
+   openDB();
    let sql = 'SELECT time_recieved, duck_id, message_id, payload FROM clusterData DESC LIMIT ?'
 
    db.all(sql, [req.params.count], (err, rows) => {
@@ -75,7 +85,27 @@ router.get('/data/latest/:count', function (req, res, next) {
 
       res.json(rows);
    });
+
+   closeDB();
 });
+
+function openDB() {
+   db = new sqlite3.Database('data.db', (err) => {
+      if (err) {
+         console.error(err.message);
+      }
+      console.log('Connected to data.db');
+   });
+}
+
+function closeDB() {
+   db.close((err) => {
+      if (err) {
+         return console.error(err.message);
+      }
+      console.log('Close the database connection.');
+   });
+}
 
 
 module.exports = router;
