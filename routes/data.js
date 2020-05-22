316e15c95f8a5c836e5eaf9bc9a6c85e0f02b316
nfeuer <nickfeuer@gmail.com>
2020-05-22 12:06:18 -0400

    new queries

--- routes/data.js
+++ routes/data.js
@@ -11,10 +11,51 @@ let db = new sqlite3.Database('data.db', (err) => {
 
 printAllData();
 
+// CREATE TABLE clusterData(time_recieved datetime, duck_id TEXT, message_id TEXT, payload TEXT, path TEXT);
+
 /* GET data route */
 router.get('/', function(req, res, next) {
   res.render('data', { title: 'Express' });
 });
+
+function printAllData() {
+  let sql = 'SELECT * FROM clusterData'
+  
+  db.all(sql, [], (err, rows) => {
+    if(err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  });
+}
+
+function printDuckAllIds() {
+  let sql = 'SELECT DISTINCT duck_id FROM clusterData ORDER BY ORDER BY duck_id'
+  
+  db.all(sql, [], (err, rows) => {
+    if(err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  });
+}
+
+function printAllDuckIdData(duckId) {
+  let sql = 'SELECT DuckId duck_id FROM clusterData WHERE DuckId = ?'
+  
+  db.all(sql, [duckId], (err, rows) => {
+    if(err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  });
+}
 
 function printAllData() {
   let sql = 'SELECT * FROM clusterData'
