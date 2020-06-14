502eaa8d319a95355005b165a18174dacefefc0b
Timo Wielink <38896563+TimoWielink@users.noreply.github.com>
2020-06-14 20:55:45 +0200

    Update serialPi.py

--- serialPi.py
+++ serialPi.py
@@ -1,14 +1,14 @@
-from time import gmtime, strftime
-import sqlite3
+# from time import gmtime, strftime
+# import sqlite3
 import serial
-from sqlite3 import Error
-import json
+# from sqlite3 import Error
+# import json
 
-dbFile = "data.db"
+# dbFile = "data.db"
 
   
-ser = serial.Serial('/dev/ttyUSB0',115200)
-theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+# ser = serial.Serial('/dev/ttyUSB0',115200)
+# theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 
 while True:
@@ -22,22 +22,22 @@ while True:
 
 
 
-def writeToDb(theTime, duckId, messageId, payload, path):
-    conn = sqlite3.connect(dbFile)
-    c = conn.cursor()
-    print ("Writing to db...")
-    try:
-        c.execute("INSERT INTO clusterData VALUES (?,?,?,?,?)", (theTime, duckId, messageId, payload, path))
-        conn.commit()
-        conn.close()
-    except Error as e:
-        print(e)
+# def writeToDb(theTime, duckId, messageId, payload, path):
+#     conn = sqlite3.connect(dbFile)
+#     c = conn.cursor()
+#     print ("Writing to db...")
+#     try:
+#         c.execute("INSERT INTO clusterData VALUES (?,?,?,?,?)", (theTime, duckId, messageId, payload, path))
+#         conn.commit()
+#         conn.close()
+#     except Error as e:
+#         print(e)
 
 
-try:
-    db = sqlite3.connect(dbFile)
-    db.cursor().execute("CREATE TABLE IF NOT EXISTS clusterData (timestamp datetime, duck_id TEXT, message_id TEXT, payload TEXT, path TEXT)")
-    db.commit()
-    db.close()
-except  Error as e:
-    print(e)
+# try:
+#     db = sqlite3.connect(dbFile)
+#     db.cursor().execute("CREATE TABLE IF NOT EXISTS clusterData (timestamp datetime, duck_id TEXT, message_id TEXT, payload TEXT, path TEXT)")
+#     db.commit()
+#     db.close()
+# except  Error as e:
+#     print(e)
