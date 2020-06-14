6cf9fee00f14644c66a3463224e65adc557442be
Timo Wielink <38896563+TimoWielink@users.noreply.github.com>
2020-06-14 18:50:46 +0200

    Update serialPi.py

--- serialPi.py
+++ serialPi.py
@@ -13,11 +13,12 @@ theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 while True:
      read_serial=ser.readline()
+     print(read_serial)
      result = (theTime + "\t" + str(read_serial.payload))
      text = json.loads(read_serial.payload)
      print (json.dumps(text))
      writeToDb(theTime, text["DeviceID"], text["MessageID"], text["Payload"], text["path"])
-     print(read_serial)
+  
 
 
 
