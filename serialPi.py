d4669ecf4d54b7c8ef3894fcfb6148c29acff44d
Timo Wielink <38896563+TimoWielink@users.noreply.github.com>
2020-06-15 22:39:10 +0200

    Update serialPi.py

--- serialPi.py
+++ serialPi.py
@@ -25,7 +25,7 @@ def writeToDb(theTime, duckId, messageId, payload, path):
 while True:
     theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     payload = ser.readline()
-    print(payload.rstrip())
+#     print(payload.rstrip())
     if len(payload) >0:
       print(payload.decode('utf8'))
 #       a = json.loads(payload.decode('utf8').replace("'", '"'))
