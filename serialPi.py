955e003056cb2431e3a1be1601c45d02f2d83aa3
Timo Wielink <38896563+TimoWielink@users.noreply.github.com>
2020-06-15 22:40:15 +0200

    Update serialPi.py

--- serialPi.py
+++ serialPi.py
@@ -26,7 +26,7 @@ while True:
     theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     payload = ser.readline()
 #     print(payload.rstrip())
-    if len(payload) >0:
+    if len(payload.decode('utf8')) >0:
       print(payload.decode('utf8'))
 #       a = json.loads(payload.decode('utf8').replace("'", '"'))
 
