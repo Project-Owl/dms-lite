383f247c0f747f0ffedf2046a09c62563ed6fd25
Timo Wielink <38896563+TimoWielink@users.noreply.github.com>
2020-06-15 21:01:43 +0200

    Update serialPi.py

--- serialPi.py
+++ serialPi.py
@@ -26,7 +26,7 @@ while True:
     theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     payload = ser.readline()
     print(payload.decode('utf8'))
-    p = json.loads(payload.decode('utf8'))
+    p = payload.decode('utf8')
     writeToDb(theTime, p["DeviceID"], p["MessageID"], p["Payload"], p["path"])
 
 try:
