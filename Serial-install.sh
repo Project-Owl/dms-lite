d67c12e2036b4cf97b8801ac4ea61bb177d3cf3d
Evilgeniusnerd <kd2ael@gmail.com>
2020-07-04 18:47:57 -0400

    Install & Launch Update Fix
    Signed-off-by: Evilgeniusnerd <kd2ael@gmail.com>

--- Serial-install.sh
+++ Serial-install.sh
@@ -5,12 +5,8 @@
 #######################################
 
 ## Update packages ##
-
 echo '##updating packages' 
 sudo apt-get update -y 
-
-echo '##upgrading all packages now...'
-sudo apt-get upgrade -y
 
 echo '##installing Node Js now..'
 curl -fsSL https://deb.nodesource.com/setup_10.x | sudo bash -
@@ -34,6 +30,7 @@ lxterminal -e "python serial_sqlwriter.py"&
 
 echo '##launch chrome in kiosk mode'
 # Start Chromium in kiosk mode
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Lo$
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"$
-chromium-browser --disable-infobars --kiosk 'http://localhost:3000'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:3000 &
