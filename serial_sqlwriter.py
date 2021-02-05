93e6b5f8147a1b61409763b8e774450c7c2e530b
TimoWielink <timowielink@gmail.com>
2021-02-05 16:20:58 +0100

    build new data packet

--- serial_sqlwriter.py
+++ serial_sqlwriter.py
@@ -16,24 +16,24 @@ def writeToDb(theTime, duckId, messageId, payload, path):
     c = conn.cursor()
     print ("Writing to db...")
     try:
-        c.execute("INSERT INTO clusterData VALUES (?,?,?,?,?)", (theTime, duckId, messageId, payload, path))
+        c.execute("INSERT INTO clusterData VALUES (?,?,?,?,?,?,?)", (theTime, duckId, messageId, payload, path, hops, duckType))
         conn.commit()
         conn.close()
     except Error as e:
         print(e)
           
 while True:
-    theTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    theTime = strftime("%Y-%m-%d %Hs:%M:%S", gmtime())
     payload = ser.readline()
     prstrip = payload.rstrip().decode('utf8')
     if len(prstrip) >0:
       print(prstrip)
       try:
         p = json.loads(prstrip)
-        writeToDb(theTime, p["DeviceID"], p["MessageID"], p["Payload"], p["path"])
+        writeToDb(theTime, p["DeviceID"], p["MessageID"], p["Payload"], p["path"],p["hops"],p["duckType"])
       except:
          print(prstrip)
-         print("NOT CORRECT PACKET")
+         print("Invalid Packet")
         
         
       
@@ -41,7 +41,7 @@ while True:
       
 try:
     db = sqlite3.connect(dbFile)
-    db.cursor().execute("CREATE TABLE IF NOT EXISTS clusterData (timestamp datetime, duck_id TEXT, message_id TEXT, payload TEXT, path TEXT)")
+    db.cursor().execute("CREATE TABLE IF NOT EXISTS clusterData (timestamp datetime, duck_id TEXT, message_id TEXT, payload TEXT, path TEXT, hops INT, duck_type INT)")
     db.commit()
     db.close()
 except  Error as e:
