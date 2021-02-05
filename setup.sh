4aa28ee9aaa7401636a40ec3b76e1f955138281d
Charlie Evans <charlie.glenn.evans.jr@gmail.com>
2021-02-04 20:50:39 -0600

    Working on a new setup process
    Signed-off-by: Charlie Evans <charlie.glenn.evans.jr@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#######################################
+# Bash script to install dependencies for Project Owl Dms-lite
+# Written by @hawk4031
+#######################################
+
+if [ -f "~/.dms/setupRun" ]; then
+	echo "This setup has already been run. Exiting."
+	return 0
+fi
+
+echo "Changing default python to python3 for current user"
+grep -qxF 'alias python="/usr/bin/python3"' ~/.bashrc || echo 'alias python="/usr/bin/python3"' >> ~/.bashrc
+source ~/.bashrc
+
+echo "Gathering NodeJS information"
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo bash -
+
+echo "Updating package databases"
+sudo apt update -y
+
+echo "Upgrading packages"
+sudo apt upgrade -y
+
+echo "Installing DMS Lite dependencies"
+sudo apt install nginx sqlite3 mosquitto mosquitto-clients python3-pip nodejs -y
+
+sudo -H pip3 install --upgrade pip
+pip3 install paho-mqtt sqlite3
+
+npm install sqlite3
+npm install
+
+echo "Configuring systemd services"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Replace REPLACEPATH in service files and output a copy to /etc/systemd/system
+sudo sed -i 's/REPLACEPATH/$DIR/g' dms-lite.service > /etc/systemd/system/dms-lite.service
+sudo sed -i 's/REPLACEPATH/$DIR/g' dms-serial-python-writer.service > /etc/systemd/system/dms-serial-python-writer.service
+sudo sed -i 's/REPLACEPATH/$DIR/g' dms-wifi-python-writer.service > /etc/systemd/system/dms-wifi-python-writer.service
+
+sudo systemctl daemon-reload
+sudo systemctl start dms-lite
+
+# After this initial setup is all completed, let's create this file
+# The presence of it means we have already run this script and it
+# shouldn't be run again. This is checked at the beginning of this
+# script.
+mkdir ~/.dms
+touch ~/.dms/setupRun
