17d6ce9a1f89d1fd5ff509865291cee95be20b72
TimoWielink <timowielink@gmail.com>
2021-02-08 15:44:47 +0100

    fix wifi script

--- Wifi_sqlwriter.py
+++ Wifi_sqlwriter.py
@@ -24,7 +24,7 @@ def on_message(client, userdata, msg):
     print(msg.topic + ":\t" + result)
     # if (msg.topic == status_topic):
     p = json.loads(msg.payload)
-    print (json.dumps(text))
+    print (json.dumps(p))
     print("New message recieved")
     writeToDb(theTime, p["DeviceID"], p["MessageID"], p["Payload"], p["path"],p["hops"],p["duckType"])
     return
