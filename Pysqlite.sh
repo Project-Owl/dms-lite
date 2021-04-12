8e132a829064cecc81d46d2ac06c773d1d455fe5
Evilgeniusnerd <kd2ael@gmail.com>
2021-04-12 23:45:32 +0100

    script add and readme edits
    Signed-off-by: Evilgeniusnerd <kd2ael@gmail.com>

--- Pysqlite.sh
+++ Pysqlite.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# install SQLite 
+sudo pip3 install sqlite3
+
+#Check for db if doesn't exist create it
 sqlite3 data.db <<EOF
 CREATE TABLE IF NOT EXISTS clusterData (timestamp datetime, duck_id TEXT, topic TEXT, message_id TEXT, payload TEXT, path TEXT, hops INT, duck_type INT);
 EOF
