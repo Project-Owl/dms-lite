1b50dc83b2628c2041368fa53e848a1cef83608f
Evilgeniusnerd <kd2ael@gmail.com>
2021-05-28 20:18:17 +0100

    updated NodeJs & Patched Script
    Signed-off-by: Evilgeniusnerd <kd2ael@gmail.com>

--- Wifi-install.sh
+++ Wifi-install.sh
@@ -4,6 +4,8 @@
 # Written by @Evilgeniusnerd 
 #######################################
 
+##VERSION 1.1 ###
+
 ## Update packages ##
 
 echo '##updating packages' 
@@ -13,8 +15,8 @@ echo '##upgrading all packages now...'
 sudo apt-get upgrade -y
 
 echo '##installing Node Js now..'
-curl -fsSL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt-get install nodejs -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 echo '##installing Mosquitto...'
 sudo apt-get install mosquitto mosquitto-clients -y
@@ -27,12 +29,9 @@ echo '##installing paho-mqtt for python'
 sudo -H pip3 install --upgrade pip
 pip3 install paho-mqtt
 
-echo '##install sqlite3 for python'
-pip3 install sqlite3
-
 echo '##install node packages'
 npm install sqlite3
 npm install 
 
 echo '##install RaspAp'
-curl --fsSL https://install.raspap.com | bash -y 
+curl --fsSL https://install.raspap.com | bash 
